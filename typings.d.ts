15813a4c52ed3efcd1deefa443d17d595ec1a1fc
Damian Cyntler <damian@cyntler.com>
2022-02-08 17:52:28 +0100

    Add ts declaration file

--- typings.d.ts
+++ typings.d.ts
@@ -1,1 +1,9 @@
 declare module 'find-project-root';
+
+// Workaround related to: https://github.com/vercel/next.js/issues/29788
+declare type StaticImageData = {
+  src: string;
+  height: number;
+  width: number;
+  placeholder?: string;
+};
