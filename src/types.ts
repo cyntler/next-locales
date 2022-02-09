6dd8009a6b887eb0b5b04d60ada6701e04e6b478
Damian Cyntler <damian@cyntler.com>
2022-02-09 21:48:17 +0100

    Add types

---
+++ src/types.ts
@@ -0,0 +1,21 @@
+export interface LocalesContextValue {
+  locales: Locale[];
+  defaultLocale: string;
+}
+
+export interface Locale {
+  name: string;
+  translations: Translations;
+}
+
+export type Translations = { [key: string]: Translations | string };
+
+export interface NextLocaleAppProps {
+  nextLocales: Locale[];
+}
+
+export type TranslateValues = { [key: string]: unknown };
+
+export interface AppWithLocalesOptions {
+  isBrowserDetection: boolean;
+}
