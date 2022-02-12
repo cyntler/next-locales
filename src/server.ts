5f0758b7aba9aab2f1bcb9e09510b052bc138da3
Damian Cyntler <damian@cyntler.com>
2022-02-12 17:03:57 +0100

    Update server side export file

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 export { nextLocalesConfig } from './hocs/nextLocalesConfig';
+export { getLangAttrValue } from './utils/getLangAttrValue';
 export {
   withLocalesStaticProps,
   withLocalesStaticPaths,
 } from './hocs/withLocalesStatic';
-export { getLangAttrValue } from './utils/getLangAttrValue';
