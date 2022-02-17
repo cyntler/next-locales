daee6dc93fad37c2e47f01d0b3a012f5c59e930e
Damian Cyntler <damian@cyntler.com>
2022-02-17 20:26:43 +0100

    Add export files

---
+++ src/exports/index.ts
@@ -0,0 +1,3 @@
+export { withLocales } from '../hocs/withLocales';
+export { useTranslation } from '../hooks/useTranslation';
+export { createInitialIndexPage } from '../utils/createInitialIndexPage';
---
+++ src/exports/server.ts
@@ -0,0 +1,4 @@
+export { withLocalesConfig } from '../hofs/withLocalesConfig';
+export { getLangAttrValue } from '../utils/getLangAttrValue';
+export { withLocalesStaticProps } from '../hofs/withLocalesStaticProps';
+export { withLocalesStaticPaths } from '../hofs/withLocalesStaticPaths';
