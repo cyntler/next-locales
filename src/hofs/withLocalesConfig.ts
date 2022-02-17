3105ee48225d3d5ea1f4d1ee106edd687955c675
Damian Cyntler <damian@cyntler.com>
2022-02-17 20:27:00 +0100

    Add withLocalesConfig HOF

---
+++ src/hofs/withLocalesConfig.ts
@@ -0,0 +1,46 @@
+import { NextConfig } from 'next';
+import { lstatSync, readlinkSync } from 'fs';
+
+import { getConfig } from '../utils/getConfig';
+import { join } from 'path';
+import { getPackagePath } from '../utils/getPackagePath';
+import { replaceLocaleConsts } from '../utils/replaceLocaleConsts';
+import { getSymlinkAbsolutePath } from '../utils/getSymlinkAbsolutePath';
+
+export const withLocalesConfig = (nextConfig: NextConfig): NextConfig => ({
+  ...nextConfig,
+  trailingSlash: true,
+  webpack: (config, context) => {
+    nextConfig.webpack ? nextConfig.webpack(config, context) : {};
+
+    if (context.isServer) {
+      const { locales, defaultLocale } = getConfig();
+
+      if (locales && defaultLocale) {
+        const packagePath = getPackagePath();
+        const stat = lstatSync(packagePath);
+
+        const jsFilePath = 'dist/hocs/withLocales.js';
+
+        if (stat?.isSymbolicLink()) {
+          const symlink = readlinkSync(packagePath);
+          if (symlink) {
+            replaceLocaleConsts(
+              join(getSymlinkAbsolutePath(symlink), jsFilePath),
+              locales,
+              defaultLocale,
+            );
+          }
+        } else {
+          replaceLocaleConsts(
+            join(packagePath, jsFilePath),
+            locales,
+            defaultLocale,
+          );
+        }
+      }
+    }
+
+    return config;
+  },
+});
