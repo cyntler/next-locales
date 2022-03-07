d4fa4a01bcee878faf886d8f2238f1ec7c6b4b91
Damian Cyntler <damian@cyntler.com>
2022-03-07 22:45:05 +0100

    Fix createLangDetector test

--- src/utils/__tests__/createLangDetector.test.ts
+++ src/utils/__tests__/createLangDetector.test.ts
@@ -3,5 +3,5 @@ import { createLangDetector } from '../createLangDetector';
 test('it returns next-language-detector detect method', () => {
   const result = createLangDetector([], '');
 
-  expect(result.detect).toBeTruthy();
+  expect(result.detect).toBeDefined();
 });
