6636374436487bab56cf71adbfd233579d67e66f
Damian Cyntler <damian@cyntler.com>
2022-03-15 22:49:43 +0100

    Add tests for useLocalesContext

---
+++ src/hooks/__tests__/useLocalesContext.test.tsx
@@ -0,0 +1,29 @@
+/**
+ * @jest-environment jsdom
+ */
+import { render } from '@testing-library/react';
+
+import { Provider } from '../../contexts/LocalesContext';
+import { useLocalesContext } from '../useLocalesContext';
+
+const UseLocalesContextComponent = () => {
+  useLocalesContext();
+
+  return null;
+};
+
+test('render component with useLocalesContext hook without provider throws an error', () => {
+  expect(() => render(<UseLocalesContextComponent />)).toThrowError(
+    "No LocalesContext's Provider rendered.",
+  );
+});
+
+test('render component with useLocalesContext hook wit provider not throws an error', () => {
+  expect(() =>
+    render(
+      <Provider value={{ defaultLocale: '', locales: [] }}>
+        <UseLocalesContextComponent />
+      </Provider>,
+    ),
+  ).not.toThrowError();
+});
