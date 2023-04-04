0b9c771432d3dde598dbf4d412ba0f4f51fc7af5
Damian Cyntler <damian@cyntler.com>
2023-04-04 19:51:56 +0200

    fix(Link): ts props

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,15 +1,14 @@
-import React, { FunctionComponent, PropsWithChildren } from 'react';
-import NextLink, { LinkProps as NextLinkProps } from 'next/link';
+import React, {
+  ComponentProps,
+  FunctionComponent,
+  PropsWithChildren,
+} from 'react';
+import NextLink from 'next/link';
 import { useRouter } from 'next/router';
 
-interface LinkProps extends NextLinkProps {}
-
-export const Link: FunctionComponent<PropsWithChildren<LinkProps>> = ({
-  children,
-  locale,
-  href,
-  ...rest
-}) => {
+export const Link: FunctionComponent<
+  PropsWithChildren<ComponentProps<typeof NextLink>>
+> = ({ children, locale, href, ...rest }) => {
   const { query, asPath, pathname } = useRouter();
 
   const finalLocale = locale || query.locale?.toString() || '';
